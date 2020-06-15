044e4e21922fbbb3addf1adbc1bb82ecbad9cefe
asmatalukder3134 <asma.talukder@yahoo.com>
2020-06-15 15:57:52 -0400

    commit

---
+++ target/src/test/java/com/allbase/D2.java
@@ -0,0 +1,27 @@
+package com.allbase;
+
+public class D2 implements Comparable <D2>  {
+	int a=30;
+
+	public D2(int a) {
+		super();
+		this.a = a;
+	}
+
+	@Override
+	public String toString() {
+		return "D2 [a=" + a + "]";
+	}
+
+	public void B() {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+	public int compareTo(D2 args) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
